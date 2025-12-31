fc99fa8c2b9740bd9bdd919f17de9ab342e0d817
Vittorio Iacovella <vittorio.iacovella@unitn.it>
2025-12-31 12:13:48 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,8 +1,27 @@
-from sqlalchemy import Base
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Enum, Table
+from datetime import datetime, timezone
+import enum
+
+from .database import Base
+
+team_submission = Table
+
+class Submission(Base):
+    __tablename__="submissions"
+    score = Column()
+    timestamp = Column()
 
 class Team(Base):
-  __tablename__ = “teams”
-  id = Column
+    __tablename__="teams"
+    id = Column()
+    hash = Column()
+    refemail = Column()
+    timestamp = Column()
+    
+
+    
+    
 
 
 
