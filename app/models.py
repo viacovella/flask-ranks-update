5ba64ea4dcd3c412b356b8a1a2653c29df35b5fd
Vittorio Iacovella <vittorio.iacovella@unitn.it>
2025-12-31 15:42:53 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,27 +1,21 @@
 import sqlalchemy
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Enum, Table
+from sqlalchemy import Column, Integer, Float, String, DateTime, relationship
 from datetime import datetime, timezone
 import enum
 
 from .database import Base
 
-team_submission = Table
+class Team(Base):
+	__tablename__="teams"
+	id = Column(Integer, nullable=False, primary_key=True, index=True)
+	shortname = Column(String, nullable =False)
+	extendedname = Column(String)
+	refemail = Column(String, nullable=False)
+	submissions = relationship("Submission", back_populates="team")
 
 class Submission(Base):
-    __tablename__="submissions"
-    score = Column()
-    timestamp = Column()
-
-class Team(Base):
-    __tablename__="teams"
-    id = Column()
-    hash = Column()
-    refemail = Column()
-    timestamp = Column()
-    
-
-    
-    
-
-
-
+	__tablename__="submissions"
+	id = Column(Integer, nullable=False, primary_key=True, index=True)
+	score = Column(Float)
+	sent_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+	team = relationship("Team", back_populates="Submissions").   
