38ef3ea566c32a4dce9af36535d842f3259b1d1e
Vittorio Iacovella <vittorio.iacovella@unitn.it>
2026-01-01 10:17:50 +0100

    Update models.py
    Checked typos and relationships

--- app/models.py
+++ app/models.py
@@ -1,21 +1,26 @@
-import sqlalchemy
-from sqlalchemy import Column, Integer, Float, String, DateTime, relationship
+from sqlalchemy import Column, Integer, Float, String, DateTime, ForeignKey # <--- Aggiungi ForeignKey
+from sqlalchemy.orm import relationship
 from datetime import datetime, timezone
-import enum
-
 from .database import Base
 
 class Team(Base):
-	__tablename__="teams"
-	id = Column(Integer, nullable=False, primary_key=True, index=True)
-	shortname = Column(String, nullable =False)
-	extendedname = Column(String)
-	refemail = Column(String, nullable=False)
-	submissions = relationship("Submission", back_populates="team")
+    __tablename__ = "teams"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    shortname = Column(String, nullable=False)
+    extendedname = Column(String)
+    refemail = Column(String, nullable=False)
+    
+    # back population
+    submissions = relationship("Submission", back_populates="team") 
 
 class Submission(Base):
-	__tablename__="submissions"
-	id = Column(Integer, nullable=False, primary_key=True, index=True)
-	score = Column(Float)
-	sent_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
-	team = relationship("Team", back_populates="Submissions").   
+    __tablename__ = "submissions"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    score = Column(Float)
+    sent_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    
+    team_id = Column(Integer, ForeignKey("teams.id"), nullable=False) 
+
+    team = relationship("Team", back_populates="submissions")
